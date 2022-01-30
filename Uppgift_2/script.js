456a21327f31efe218b1769a5f480aff3437885a
Noritake Akechi <86470769+noriAkechi@users.noreply.github.com>
2022-01-30 17:50:59 +0100

    v.0.2

--- Uppgift_2/script.js
+++ Uppgift_2/script.js
@@ -1,10 +1,8 @@
 //------------------------------------------------------------------------------------------
 //Contact form validation
 //------------------------------------------------------------------------------------------
-import { ContactPerson } from "./classLib.js";
-
 function formValidation() {
-    //initialize variables
+    //Initialize variables
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let comment = document.getElementById("comment").value;
@@ -12,63 +10,66 @@ function formValidation() {
     let date = new Date(); 
     let today = date.toDateString();
 
-    // Det fungerar om jag definerar class:en här i script.js... 
-    //-----------------------------------------------------------
-    // let ContactPerson = class {
-    //     constructor(date, name, email, comment) {
-    //         this.date = date;
-    //         this.name = name;
-    //         this.email = email;
-    //         this.comment = comment;
-    //     }
+    //Initialize class
+    class ContactPerson {
+        constructor(date, name, email, comment) {
+            this.date = date;
+            this.name = name;
+            this.email = email;
+            this.comment = comment;
+        }
+        //Method: Confirmation
+        confirmText() {
+             let result = window.confirm("Are you really really sure???\n\n" +
+                 this.date + "\n-------------------\n" +
+                 this.name + "\n-------------------\n" +
+                 this.email + "\n-------------------\n" +
+                 this.comment + "\n-------------------\n"
+             );
+             if(result) {
+                document.getElementById("alartText").innerHTML = "Thank you! Message has been sent!";
+            }
+         }
 
-    //     confirmText() {
-    //          window.confirm("Are you really really sure???\n\n" +
-    //              this.date + "\n-------------------\n" +
-    //              this.name + "\n-------------------\n" +
-    //              this.email + "\n-------------------\n" +
-    //              this.comment + "\n-------------------\n"
-    //          );
-    //      }
-    // }
+         //Method: Send a confirmation email. 
+         sendConfirmation() {
+             //exempel
+         }
+    }
 
-    //make instance from class "ContactPerson"
-    let contactPerson = new ContactPerson(today, name, email, comment);
-    document.getElementById("chk") = contactPerson.name;
+    const contactPerson = new ContactPerson(today, name, email, comment);
 
     //Validation: user's name
-    if (contactPerson.name.length != 0){ // if not blank
+    if (name.length != 0){ // if not blank
         flag ++;
     } else {
         document.getElementById("name").placeholder = "Name field is blank";
     }
 
     // //Validation: user's Email address
-     if (contactPerson.email.length == 0) {
-         document.getElementById("email").placeholder = "Epost field is blank";
-     //Validation: Correct form or not (call external function)
-     } else if (validateEmail(email)){
-         flag ++;
-     } else {
-         alert("You have entered an invalid email address!")
-     }
+    if (email.length == 0) {
+        document.getElementById("email").placeholder = "Epost field is blank";
+    //Validation: Correct form or not (call external function)
+    } else if (validateEmail(email)){
+        flag ++;
+    } else {
+        alert("You have entered an invalid email address!")
+    }
 
     //Validation: user's comment
-    if (contactPerson.comment.length != "") { // if not blank
-         flag ++;
-     } else {
-         document.getElementById("comment").placeholder = "Massage field is blank";
-     }
+    if (comment.length != "") { // if not blank
+        flag ++;
+    } else {
+        document.getElementById("comment").placeholder = "Massage field is blank";
+    }
 
-         //If all are passed
-         if (flag == 3) {
-             //window.confirm("Are you really really sure?\n\n" + inquiry.join('\n-------------------\n'));
-             contactPerson.confirmText();
-             document.getElementById("alartText").innerHTML = "Thank you! Message has been sent!";
-         }
+    //If all are passed
+    if (flag == 3) {
+        contactPerson.confirmText();
+    }
 
+//    document.getElementById("chk").innerHTML = flag;
 }
-
 
 //------------------------------------------------------------------------------------------
 //External function for email format validation
